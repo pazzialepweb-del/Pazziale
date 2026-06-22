1f63e60bcf537aba6253f9a1945b6949380eaada
bastianwebweb-jpg <bastianwebweb@gmail.com>
2026-06-22 17:47:03 -0400

    mejora google

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,36 @@
 import '@/app/globals.css';
 import { CarritoProvider } from '@/context/CarritoContext';
-import { GoogleAnalytics } from '@next/third-parties/google';
+import Script from 'next/script';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="es">
+      <head>
+        {/* Google Analytics (gtag.js) */}
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=G-HGRCMVCBBL`}
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-HGRCMVCBBL', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
+      </head>
       <body>
         <CarritoProvider>
           {children}
         </CarritoProvider>
-        <GoogleAnalytics gaId="G-HGRCMVCBBL" />
       </body>
     </html>
   );
 }
-///--//
